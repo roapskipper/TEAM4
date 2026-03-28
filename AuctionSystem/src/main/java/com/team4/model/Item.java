2ac3a6031c7ceac4bf47989bdedba61248367e88
HaiAnh <25020020@vnu.edu.vn>
2026-03-28 20:45:54 +0700

    Thêm class Item

--- AuctionSystem/src/main/java/com/team4/model/Item.java
+++ AuctionSystem/src/main/java/com/team4/model/Item.java
@@ -1,5 +1,31 @@
 package com.team4.model;
 
-public abstract class Item {
+import java.io.Serializable;
 
+public abstract class Item implements Serializable {
+    protected String id;
+    protected String name;
+    protected double startingPrice;
+    protected double currentPrice;
+    protected String description;
+
+    public Item(String id, String name, double startingPrice, String description) {
+        this.id = id;
+        this.name = name;
+        this.startingPrice = startingPrice;
+        this.currentPrice = startingPrice;
+        this.description = description;
+    }
+
+    public abstract void showInfo();
+
+    public String getName() {
+        return name;
+    }
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
 }
