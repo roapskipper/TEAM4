6429d44ba0438cb655aec6b22bc79f61afcd438e
HaiAnh <25020020@vnu.edu.vn>
2026-03-30 22:39:48 +0700

    theem autobidding, bidobserver, auctionmanager

--- AuctionSystem/src/main/java/com/team4/observer/BidObserver.java
+++ AuctionSystem/src/main/java/com/team4/observer/BidObserver.java
@@ -1,3 +1,9 @@
 package com.team4.observer;
 
-public class BidObserver {}
+import com.team4.model.Auction;
+import com.team4.model.BidTransaction;
+
+public interface BidObserver {
+    // Hàm này sẽ tự động được gọi khi có người đặt giá mới thành công
+    void updateNewBid(Auction auction, BidTransaction latestBid);
+}
