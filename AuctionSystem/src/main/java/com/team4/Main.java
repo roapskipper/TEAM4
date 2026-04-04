49ba1fa1ddc2e75d0020d91ae88daeb062125103
Minh Trung <trantrungpro2k7@gmail.com>
2026-04-04 12:41:55 +0700

    Confirmed database schema created successfully

--- AuctionSystem/src/main/java/com/team4/Main.java
+++ AuctionSystem/src/main/java/com/team4/Main.java
@@ -1,7 +1,20 @@
 package com.team4;
 
-class Main {
-    public void main(String[] args) {
-        System.out.println("For test");
+import com.team4.db.DatabaseManager;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---- KIEM TRA HE THONG ----");
+        try {
+            // Goi Singleton cua Hai Anh
+            DatabaseManager db = DatabaseManager.getInstance();
+            if (db.testConnection()) {
+                System.out.println("[SUCCESS] Database da ket noi thanh cong!");
+            } else {
+                System.out.println("[FAIL] Vui long kiem tra mat khau tai file .properties");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
