29a49142a05803be9f3041259307872d92dbcde8
Nguyen Phuc Binh <binhnguyenlcs@gmail.com>
2026-04-18 11:57:08 +0700

    fix: xu ly co gian responsive va chong tran vien cho form dang nhap

--- src/main/java/auctionsystemclient/Main.java
+++ src/main/java/auctionsystemclient/Main.java
@@ -9,9 +9,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
         stage.setTitle("AuctionSpace - Đăng nhập");
         stage.setScene(scene);
+        stage.setMinWidth(900);
+        stage.setMinHeight(650);
         stage.show();
     }
     public static void main(String[] args) {
