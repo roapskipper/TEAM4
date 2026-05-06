f1d676a3da659c979f43f0f12929b4398c283f25
Minh Trung <trantrungpro2k7@gmail.com>
2026-05-06 12:24:57 +0700

    OK

--- AuctionSystem/src/main/java/com/team4/Main.java
+++ AuctionSystem/src/main/java/com/team4/Main.java
@@ -1,6 +1,35 @@
-import com.team4.db.DatabaseManager;
+package com.team4;
 
-public static void main(String[] args) {
-    DatabaseManager.initialize();
-    System.out.println("Setup thành công!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load man hinh dang nhap
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/com/team4/view/login.fxml")
+        );
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(
+                getClass().getResource("/com/team4/view/style.css").toExternalForm()
+        );
+
+        primaryStage.setTitle("AuctionSpace - Dang nhap");
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(1000);
+        primaryStage.setMinHeight(700);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
