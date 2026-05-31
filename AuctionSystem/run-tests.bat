82646047b972261cdf9b63bc7d4bceb5ea1691c1
roapskipper <nguyenloc0322g@gmail.com>
2026-05-31 11:13:29 +0700

    chore: move docs file & remove redundant .bat file

--- AuctionSystem/run-tests.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Script chạy tests với Java 21
-SET JAVA_HOME=C:\Program Files\Java\jdk-21
-SET PATH=%JAVA_HOME%\bin;%PATH%
-echo [INFO] Đang dùng: 
-java -version
-echo.
-echo [INFO] Chạy mvn test (exclude integration tests)...
-mvn test
